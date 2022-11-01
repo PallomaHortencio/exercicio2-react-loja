0e4a75c15bf46f645d85bf504b3bb362f32055d5
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-11-01 11:58:09 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const Produtos = () => {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function getProdutos() {
+      try {
+        const resposta = await fetch(`'https://fakestoreapi.com/products'`);
+        const dados = await resposta.json();
+
+        console.log(dados);
+        setProdutos(dados);
+      } catch (error) {
+        console.log("Deu ruim na busca do post: " + error.message);
+      }
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h2>Nossos Produtos</h2>
+    </section>
   );
-}
+};
 
-export default App;
+export default Produtos;
